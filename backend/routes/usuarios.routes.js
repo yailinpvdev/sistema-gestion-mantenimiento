846cfa1f9235e9c6a6b6b2984c08b398acd31196
Yailín <tu-correo@github.com>
2026-08-19 16:31:12 -0500

    feat: create users endpoint

--- backend/routes/usuarios.routes.js
+++ backend/routes/usuarios.routes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const pool = require("../db");
+const bcrypt = require("bcrypt");
 
 const router = express.Router();
 
@@ -14,6 +15,28 @@ router.get("/", async (req, res) => {
     console.error("Error al obtener usuarios:", error.message);
     res.status(500).json({
       mensaje: "Error al obtener usuarios",
+    });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const { nombre, correo, password, rol } = req.body;
+
+    const passwordEncriptada = await bcrypt.hash(password, 10);
+
+    const resultado = await pool.query(
+      `INSERT INTO usuarios (nombre, correo, password, rol)
+       VALUES ($1, $2, $3, $4)
+       RETURNING id, nombre, correo, rol, activo, created_at`,
+      [nombre, correo, passwordEncriptada, rol],
+    );
+
+    res.status(201).json(resultado.rows[0]);
+  } catch (error) {
+    console.error("Error al crear usuario:", error.message);
+    res.status(500).json({
+      mensaje: "Error al crear usuario",
     });
   }
 });
